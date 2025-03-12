8d437a3b7586f8da009064b29cdcaf00c1505390
Christian <chrisdaq@gmail.com>
2025-03-12 16:51:53 -0400

    test toast async component, extend vitest config to adding matchMedia support

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,3 +1,18 @@
 // add more matchers
 // github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/vitest';
+
+// match media not found fix
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+})
