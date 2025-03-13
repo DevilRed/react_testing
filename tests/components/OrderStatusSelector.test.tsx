8041cf36649dc6b8162fc89c0ac9bbe3a935e3c7
Christian <chrisdaq@gmail.com>
2025-03-12 21:58:22 -0400

    test events when selecting options on order status selector

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -4,13 +4,15 @@ import { Theme } from '@radix-ui/themes';
 import userEvent from '@testing-library/user-event';
 
 describe('OrderStatusSelector', () => {
-	const renderComponent = () => {
+	const renderComponent = (cb=vi.fn()) => {
 		render(<Theme>
-				<OrderStatusSelector onChange={vi.fn()}/>
+				<OrderStatusSelector onChange={cb}/>
 			</Theme>)
 		return {
 			button: screen.getByRole('combobox'),
-			getOptions: () => screen.findAllByRole('option')
+			getOptions: () => screen.findAllByRole('option'),
+			getOption: (label: RegExp) => screen.findByRole('option', { name: label}),
+			user: userEvent.setup()
 		}
 	}
 	it('should render new as default value', () => {
@@ -19,8 +21,7 @@ describe('OrderStatusSelector', () => {
 	});
 
 	it('should render correct statuses', async () => {
-		const user = userEvent.setup()
-		const { button, getOptions } = renderComponent()
+		const { button, getOptions, user } = renderComponent()
 		await user.click(button)
 
 		const options = await getOptions()
@@ -29,5 +30,38 @@ describe('OrderStatusSelector', () => {
 		// test options have right labels
 		const labels = options.map(option => option.textContent)
 		expect(labels).toEqual(['New', 'Processed', 'Fulfilled'])
+	});
+
+	// using each as data provider
+	it.each([
+		{ label: /processed/i, value: 'processed' },
+		{ label: /fulfilled/i, value: 'fulfilled' },
+	])
+	('should call onChange with $value when the $label option is selected', async ({ label, value }) => {
+		const myCb = vi.fn();
+		const { button, user, getOption } = renderComponent(myCb)
+		await user.click(button)
+
+		const option = await getOption(label)
+		await user.click(option)
+
+		// test options have right labels
+		expect(myCb).toHaveBeenCalledWith(value)
+	});
+
+	// new option is default value so click on other option first then get back to new option
+	it("should call onChange with 'new' when the New option is selected", async () => {
+		const myCb = vi.fn();
+		const { button, user, getOption } = renderComponent(myCb)
+		await user.click(button)
+
+		const processedOption = await getOption(/processed/i)
+		await user.click(processedOption)
+
+		await user.click(button)
+		const newOption = await getOption(/new/i)
+		await user.click(newOption)
+
+		expect(myCb).toHaveBeenCalledWith('new')
 	});
 })
