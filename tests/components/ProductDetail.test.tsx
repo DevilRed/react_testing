9914729db9e727355138ff8051d67a2504f71b34
Christian <chrisdaq@gmail.com>
2025-03-13 17:05:09 -0400

    fix test by creating required data reading the db before all test

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,14 +1,23 @@
-import { render, screen } from '@testing-library/react'
-import ProductDetail from '../../src/components/ProductDetail';
-import { products } from '../mocks/data';
-import { server } from '../mocks/server';
+import { render, screen } from '@testing-library/react';
 import { http, HttpResponse } from "msw";
+import ProductDetail from '../../src/components/ProductDetail';
+import { db } from '../mocks/db';
+import { server } from '../mocks/server';
 
 describe('ProductDetail', () => {
+	let productId: number;
+	beforeAll(() => {
+		const product = db.product.create()
+		productId = product.id
+	})
+	afterAll(() => {
+		db.product.delete({ where: { id: { equals: productId }}})
+	})
 	it('should render the product', async() => {
-		render(<ProductDetail productId={1} />)
-		expect(await screen.findByText(new RegExp(products[0].name))).toBeInTheDocument()
-		expect(await screen.findByText(new RegExp(products[0].price.toString()))).toBeInTheDocument()
+		const product = db.product.findFirst({where: { id: {equals: productId}}})
+		render(<ProductDetail productId={productId} />)
+		expect(await screen.findByText(new RegExp(product!.name))).toBeInTheDocument()
+		expect(await screen.findByText(new RegExp(product!.price.toString()))).toBeInTheDocument()
 	});
 
 	it('should render message if product is not found', async() => {
