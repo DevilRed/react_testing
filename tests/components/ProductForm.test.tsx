a81eb1ef0f29aeb5856143b47f6a5ff9e60efabc
Christian <chrisdaq@gmail.com>
2025-03-19 14:52:19 -0400

    test product form

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -1,15 +1,44 @@
 import { render, screen } from "@testing-library/react";
 import ProductForm from "../../src/components/ProductForm";
+import { Category, Product } from "../../src/entities";
 import AllProviders from "../AllProviders";
+import { db } from "../mocks/db";
 
 describe("ProductForm", () => {
+  let category: Category;
+  beforeAll(() => {
+    category = db.category.create();
+  });
+  afterAll(() => {
+    db.category.delete({ where: { id: { equals: category.id } } });
+  });
   it("should render form fields", async () => {
     render(<ProductForm onSubmit={vi.fn()} />, { wrapper: AllProviders });
+    // await for form element to be rendered
     await screen.findByRole("form");
     expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument();
     expect(screen.getByPlaceholderText(/price/i)).toBeInTheDocument();
     expect(
       screen.getByRole("combobox", { name: /category/i })
     ).toBeInTheDocument();
+  });
+  it("should render product data when editing", async () => {
+    const product: Product = {
+      id: 1,
+      name: "Product 1",
+      price: 100,
+      categoryId: 1,
+    };
+    render(<ProductForm onSubmit={vi.fn()} product={product} />, {
+      wrapper: AllProviders,
+    });
+    await screen.findByRole("form");
+    expect(screen.getByPlaceholderText(/name/i)).toHaveValue(product.name);
+    expect(screen.getByPlaceholderText(/price/i)).toHaveValue(
+      product.price.toString()
+    );
+    expect(
+      screen.getByRole("combobox", { name: /category/i })
+    ).toHaveTextContent(category.name);
   });
 });
