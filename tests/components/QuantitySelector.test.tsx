5f9c16aba69483753f295f58153d2d90d6dec789
Christian <chrisdaq@gmail.com>
2025-03-22 10:52:54 -0400

    add product to the cart, return more elements from renderedComponent function, return them  using arrow function because those elements dont exist when the component is rendered, they appear after clicking on the add to cart button

--- tests/components/QuantitySelector.test.tsx
+++ tests/components/QuantitySelector.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react'
 import { CartProvider } from '../../src/providers/CartProvider';
 import QuantitySelector from '../../src/components/QuantitySelector';
 import { Product } from '../../src/entities';
+import userEvent from '@testing-library/user-event';
 
 describe('QuantitySelector', () => {
 	const renderComponent = () => {
@@ -18,11 +19,27 @@ describe('QuantitySelector', () => {
 		)
 
 		return {
-			addToCartButton: screen.getByRole('button', {name: /add to cart/i})
+			addToCartButton: screen.getByRole('button', {name: /add to cart/i}),
+			user: userEvent.setup(),
+			// initially when component is rendered these elements don't exist
+			getQuantity: () =>  screen.getByRole('status'),
+			getDecrementButton: () =>  screen.getByRole('button', { name: '-'}),
+			getIncrementButton: () =>  screen.getByRole('button', { name: '+'})
 		}
 	}
 	it('should render the add to cart button', () => {
 		const {addToCartButton} = renderComponent()
 		expect(addToCartButton).toBeInTheDocument()
+	});
+
+	it('should add the product to the cart',async () => {
+		const {addToCartButton, user, getQuantity, getDecrementButton, getIncrementButton} = renderComponent()
+		await user.click(addToCartButton)
+		expect(getQuantity()).toHaveTextContent('1')
+
+		expect(getDecrementButton()).toBeInTheDocument()
+		expect(getIncrementButton()).toBeInTheDocument()
+
+		expect(addToCartButton).not.toBeInTheDocument()
 	});
 })
